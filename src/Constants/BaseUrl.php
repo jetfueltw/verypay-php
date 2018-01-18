9de8ca4c56bd950e49c13beba165afd28aa4ae8e
Greene Liu <greene@jetfuel.com.tw>
2018-01-18 21:03:27 +0800

    more channel BaseUrl

--- src/Constants/BaseUrl.php
+++ src/Constants/BaseUrl.php
@@ -6,8 +6,11 @@ class BaseUrl
 {
     const TRADE_QUERY     = 'http://payquery.qyfpay.com:90/';
     const DIGITAL_PAYMENT = [
-        Channel::WECHAT => 'http://wx.qyfpay.com:90/',
-        Channel::ALIPAY => 'http://zfb.qyfpay.com:90/',
-        Channel::QQ     => 'http://qq.qyfpay.com:90/',
+        Channel::WECHAT   => 'http://wx.qyfpay.com:90/',
+        Channel::ALIPAY   => 'http://zfb.qyfpay.com:90/',
+        Channel::QQ       => 'http://qq.qyfpay.com:90/',
+        Channel::JDPAY    => 'http://jd.qyfpay.com:90/',
+        Channel::UNIONPAY => 'http://unionpay.qyfpay.com:90/',
+        Channel::BAIDU    => 'http://baidu.qyfpay.com:90/',
     ];
 }
